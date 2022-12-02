60d086852775f7702581f92ccb4f0b9281ea9a55
paulbouvignies <38011745+paulbouvignies@users.noreply.github.com>
2022-12-02 11:23:15 +0100

    create global script

--- design-system/src/stories/BaseInput.tsx
+++ design-system/src/stories/BaseInput.tsx
@@ -1,5 +1,5 @@
 
-import React, { useId } from "react";
+import React from "react";
 
 
 type Props =  {
@@ -12,13 +12,21 @@ type Props =  {
   value?: string;
 };
 
+// generate a random id for the input
+const generateUUID = ():string => {
+  return "xx-4x-yxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 
 
 export const BaseInput:React.FC<Props> = (props) => {
   const { label, type, ...rest } = props;
   const [isFocus, setIsFocus] = React.useState(false);
-  const id = useId();
+  const id = generateUUID();
 
 
 
